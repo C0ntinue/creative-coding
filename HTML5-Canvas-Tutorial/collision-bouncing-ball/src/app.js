9eaf10e7beae2cad5171b0b0a4535b201b895a00
sehee yun <seheeyun29@gmail.com>
2021-01-22 00:35:56 +0900

    🎉 moving wave 추가

--- HTML5-Canvas-Tutorial/collision-bouncing-ball/src/app.js
+++ HTML5-Canvas-Tutorial/collision-bouncing-ball/src/app.js
@@ -6,7 +6,7 @@ class App {
     this.canvas = document.getElementById('canvas');
     this.ctx = this.canvas.getContext('2d');
 
-    window.addEventListener('resize', this.resize, false);
+    window.addEventListener('resize', this.resize, false); // 가변적인 브라우저를 위해 리사이즈를 해서 크기를 구함
     this.resize();
 
     this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 15);
@@ -27,7 +27,7 @@ class App {
   animate = t => {
     window.requestAnimationFrame(this.animate);
 
-    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight); // 그려져있는 item을 지워서 하나의 원만 남게함
 
     this.block.draw(this.ctx);
     this.ball.draw(this.ctx, this.stageWidth, this.stageHeight, this.block);
